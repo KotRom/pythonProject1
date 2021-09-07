37ebb43b946a28a77215af1bf0e499f420024c03
Dmitri <86003479+ArthurIpolitov@users.noreply.github.com>
2021-09-07 19:51:41 +0300

    First commit and push

--- tester.py
+++ tester.py
@@ -1,2 +1,74 @@
-name = input("enter_name:")
-print(name)
+# # first_name = "Roman"
+# # last_name = "Kotenjov"
+# # birth_date = "15.12.1979"
+# print(type("hello"))
+# x = 5
+# y = 10.5
+# print(x)
+# print(type(x))
+#
+# print(y)
+# print(type(y))
+#
+# print(2+2*2)
+# print(x+y+10*x)
+#
+# z = "hello"
+# print(z + z)
+# print(type(z + z))
+#
+# print('a'*10)
+#
+# m = x + y
+# print(m)
+
+# x = True (Boolean)
+# y = False(Boolean)
+# print("hello" == "Hello")
+# print(type(x))
+# x = None
+
+# x = "hello"
+# y = 6
+# z = str(y)
+# print(x + z)
+
+# user_name = input("Enter your name: ")
+# user_age = int(input("Enter age: "))
+# birth_year = str(2021 - user_age)
+# print("Hello " + user_name + ". You was born in " + birth_year)
+# print(type(user_age))
+# print(type(birth_year))
+
+some_int = 5
+some_float = 5.5
+some_string = "123456.7890"
+print(type(float(some_string)))
+print(int(float(some_string)))
+
+some_bool = True
+print(int(some_bool))
+
+print(bool(23232))
+print(bool(0))
+
+string1 = 'Hello "Roman"' \
+    'How are you?'
+srting2 = """Hello Roman
+How are you?"""
+print(srting2)
+print(string1)
+
+side_a = float(input("enter side a: "))
+side_b = float(input("enter side b: "))
+side_c = float(input("enter side c: "))
+half_perimeter = (side_a + side_b + side_c) / 2
+triangle_area = (half_perimeter * (half_perimeter - side_a) *
+                (half_perimeter - side_b) *
+                (half_perimeter - side_c)) ** 0.5
+
+print("Side A =", side_a) # Side A is 3.
+print("Side a = " + str(side_a))
+print("Side b = " + str(side_b))
+print("Side c = " + str(side_c))
+print("Triangle area = " + str(triangle_area))
