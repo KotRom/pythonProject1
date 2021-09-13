da75f258a83df840d8ae9e9eadac920c9767b78f
mhaitov <81020414+mhaitov@users.noreply.github.com>
2021-09-13 16:00:30 +0300

    13.09.2021

--- 003_if_elif_else.py
+++ 003_if_elif_else.py
@@ -1,3 +1,20 @@
 x = 5
 if x < 10:
     print(x)
+
+
+y = 11
+if y == 11:
+     print(f'{y} is equal to 11')
+elif y > 11:
+     print(f"{y} is mmenshe 11)")
+else:
+     print("FU")
+
+id_code = input(("Please enter id: "))
+if len(id_code == 11):
+         print("id code is correct")
+         if id_code[0] == 3:
+             print("You are nmale")
+         elif id_code[0] == 4:
+             print("you are female")
