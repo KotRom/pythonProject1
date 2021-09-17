d42a8935b1f9933f30d0cc92d8656a49d0f46376
mhaitov <81020414+mhaitov@users.noreply.github.com>
2021-09-17 20:07:05 +0300

    17.09.2021

---
+++ 011_pass.py
@@ -0,0 +1,13 @@
+#for num in range(0, 100):
+#    pass prosto propustit'
+while True:
+    user_input = input("please choose: 1 to print or 2 to exit: ")
+    if user_input == '1':
+        print('Hello world')
+        continue
+    elif user_input == '2':
+        break
+    else:
+        pass
+    print('out of statement')
+
