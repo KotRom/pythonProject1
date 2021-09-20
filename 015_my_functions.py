748d12ffedcc04c53188e05933fd88ead3d89f79
mhaitov <81020414+mhaitov@users.noreply.github.com>
2021-09-20 14:43:55 +0300

    20.09.2021

--- 015_my_functions.py
+++ 015_my_functions.py
@@ -1,6 +1,14 @@
-import my_functions
+# import my_functions
+#workers = [('Jack', 'Smith', 2500, 11), ('Mary', 'Brown', 3000, 25), ('John', 'Brown', 7000, 34)]
+#for person in workers:
+#    my_functions.print_employer_message(person)
+
+from my_functions import print_employer_message, return_x # * - import all functions from file
+
+import my_functions as mf
 
 workers = [('Jack', 'Smith', 2500, 11), ('Mary', 'Brown', 3000, 25), ('John', 'Brown', 7000, 34)]
 
 for person in workers:
-    my_functions.print_employer_message(person)
+    print_employer_message(person)
+#    mf.print_employer_message(person)
