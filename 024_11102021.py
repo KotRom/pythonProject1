5d8f640aff59550239d0af90383dd1f339b4546a
Dmitri <86003479+ArthurIpolitov@users.noreply.github.com>
2021-10-11 14:37:40 +0300

    11.10.2021

--- 024_11102021.py
+++ 024_11102021.py
@@ -2,9 +2,28 @@
 
 #c:\users\37255\appdata\local\programs\python\python39new\python.exe -m pip install --upgrade pip
 
-import mysql_connector
-mydb = mysql_connector.connect(
+import mysql.connector
+import  pandas as pd
+
+mydb = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='1234'
+    password='12345',
+    database='test_db',
 )
+mycursor = mydb.cursor()
+mycursor.execute(f'SELECT * FROM students')
+for student in mycursor:
+    print(student)
+
+#mycursor.execute("CREATE TABLE student (name VARCHAR(255), age INT(10))")
+
+#sqlFormula = "INSERT INTO students (name, age) VALUES ('Roman', '33')"
+#mycursor.execute(sqlFormula)
+#mydb.commit()
+
+#mycursor.execute('CREATE DATABASE test_db')
+#mycursor.execute('SHOW DATABASES')
+#for db in mycursor:
+#    print(db)
+
