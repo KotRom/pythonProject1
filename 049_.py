9cc955694b00d9058b16272465aa508c2c61ba4f
Dmitri <86003479+ArthurIpolitov@users.noreply.github.com>
2021-10-26 19:20:39 +0300

    26.10

---
+++ 049_.py
@@ -0,0 +1,15 @@
+# pip install speedtest
+# wikipedia
+# pymovie
+# django
+# opencv
+
+#micropython.org dlja upravlenie kontroleerami
+
+#scrapy web scrapping
+#pillow dlja raboty s izobrazhenijami
+
+#https://realpython.com/lessons/arduino-python-overview/
+
+#https://create.arduino.cc/projecthub/hamzakhalidonline/python-software-for-arduino-rfid-415a91?ref=tag&ref_id=python&offset=12
+#https://www.hackster.io/nickthegreek82/arduino-rfid-lock-tutorial-51ccba
